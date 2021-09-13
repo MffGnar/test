887269e1fcd17f58128b5ee745ea029aca6c923c
MFF <1528168873@qq.com>
2021-09-13 18:31:59 +0800

    上传

---
+++ mff/1.py
@@ -0,0 +1,25 @@
+# sStr1 = 'strcpy'
+# sStr2 = 'sStr1'
+# sStr1 = 'strcpy2'
+# print( sStr2 )
+
+# a = '123'
+# b = '123'
+# print(a+b)
+#
+# sStr1 = 'strcat'
+# sStr2 = 'append'
+# sStr1 += sStr2
+# print( sStr1 )
+# print( type(16/4) )
+# a=10
+# b=20
+# print('a+b')
+# s='xyz'
+# i=2
+# print('s+i')
+# print(type(1))
+
+name = 'ggg'
+print('{}说{}'.format(name,'啦啦啦'))   #ggg说啦啦啦
+print('{}说\'啦啦啦\''.format(name,))   #ggg说'啦啦啦'  有r会保留原格式，不发生转义
